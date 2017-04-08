71abeeb6018fc33316cf97de629006f5b0495eb3
Terence Sun <tsun1215@gmail.com>
2017-04-08 17:34:14 -0400

    Updated main to use grideye.h

--- occulow-firmware/src/main.c
+++ occulow-firmware/src/main.c
@@ -29,6 +29,7 @@
  */
 #include <asf.h>
 #include <drivers/pir/pir.h>
+#include <drivers/grideye/grideye.h>
 
 /**
  * @brief      Runs when the PIR detects motion
@@ -41,9 +42,15 @@ void pir_on_wake(void) {
 
 int main (void)
 {
+	uint16_t grideye_frame[NUM_PIXELS];
 	system_init();
+	grideye_init();
 	pir_init(pir_on_wake);  // Init PIR
 
 	while(1) {
+		if (!ge_is_sleeping()) {
+			ge_get_frame(grideye_frame);
+			// TODO: Do something with grideye_frame
+		}
 	}
 }
