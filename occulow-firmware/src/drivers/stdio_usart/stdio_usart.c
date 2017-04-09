0b8c0f32ed25b3447d14203f392febbe7905da93
Terence Sun <tsun1215@gmail.com>
2017-04-08 21:40:42 -0400

    Added driver for stdio over usart

---
+++ occulow-firmware/src/drivers/stdio_usart/stdio_usart.c
@@ -0,0 +1,23 @@
+/*
+ * stdio_usart.c
+ *
+ * Created: 4/8/2017 9:24:58 PM
+ *  Author: tsun
+ */ 
+
+#include <drivers/stdio_usart/stdio_usart.h>
+
+struct usart_module stdio_usart_module;
+
+void stdio_init() {
+	struct usart_config stdio_usart_config;
+	usart_get_config_defaults(&stdio_usart_config);
+	
+	stdio_usart_config.mux_setting = STDIO_SERCOM_MUX_SETTING;
+	stdio_usart_config.pinmux_pad0 = STDIO_SERCOM_PINMUX_PAD0;
+	stdio_usart_config.pinmux_pad1 = STDIO_SERCOM_PINMUX_PAD1;
+	stdio_usart_config.pinmux_pad2 = STDIO_SERCOM_PINMUX_PAD2;
+	stdio_usart_config.pinmux_pad3 = STDIO_SERCOM_PINMUX_PAD3;
+	stdio_serial_init(&stdio_usart_module, STDIO_SERCOM_MODULE, &stdio_usart_config);
+	usart_enable(&stdio_usart_module);
+}
