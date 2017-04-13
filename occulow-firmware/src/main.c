3cb6117dcf41fac4ffae653d9387bd1e37db77b0
Terence Sun <tsun1215@gmail.com>
2017-04-13 13:50:41 -0400

    Changed print format

--- occulow-firmware/src/main.c
+++ occulow-firmware/src/main.c
@@ -30,16 +30,24 @@ int main (void)
 
 	double in_count = 0;
 	double out_count = 0;
-	
+
 	while(1) {
 		if (!ge_is_sleeping()) {
 			ge_get_frame(grideye_frame);
 			pc_new_frame(grideye_frame);
 			in_count = pc_get_in_count();
 			out_count = pc_get_out_count();
-			
+
 			if (in_count > 0.0 || out_count > 0.0) {
-				printf("In: %d, Out: %d\r\n", (int) in_count, (int) out_count);
+				if (((double) ((int) in_count)) < in_count && ((double) ((int) out_count)) < out_count) {
+					printf("D(%d.5,%d.5)\r\n", (int) in_count, (int) out_count);
+				} else if (((double) ((int) in_count)) < in_count) {
+					printf("D(%d.5,%d)\r\n", (int) in_count, (int) out_count);
+				} else if (((double) ((int) out_count)) < out_count) {
+					printf("D(%d,%d.5)\r\n", (int) in_count, (int) out_count);
+				} else {
+					printf("D(%d,%d)\r\n", (int) in_count, (int) out_count);
+				}
 			}
 		}
 	}
