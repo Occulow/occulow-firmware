85540b40fb197b51752b482de7edfef1cdeb1e7a
Terence Sun <tsun1215@gmail.com>
2017-04-16 15:35:15 -0400

    Fixed quicksort to work

--- occulow-firmware/src/drivers/people_counting/people_counting_utils.h
+++ occulow-firmware/src/drivers/people_counting/people_counting_utils.h
@@ -3,7 +3,7 @@
  *
  * Created: 4/8/2017 10:09:27 PM
  *  Author: Terence Sun (tsun1215)
- */ 
+ */
 
 
 #ifndef PEOPLE_COUNTING_UTILS_H_
@@ -23,8 +23,9 @@
 #define GET_FRAME_INDEX(ROW, COL) ((ROW) * GRID_SIZE + (COL))
 
 void enqueue_frame(frame_t *frame_queue, frame_t new_frame, uint16_t queue_size);
-uint16_t partition(frame_elem_t arr[], uint16_t low, uint16_t high);
-void quick_sort(frame_elem_t arr[], uint16_t low, uint16_t high);
+void swap (frame_elem_t *a, frame_elem_t *b);
+int16_t partition (frame_t arr, int16_t l, int16_t h);
+void quick_sort(frame_t arr, int16_t low, int16_t high);
 uint16_t median_at_index(frame_t *frames, uint16_t num_frames, uint16_t index);
 frame_t compute_median_frame(frame_t frame_out, frame_t *frames, uint16_t num_frames);
 bool is_local_max(frame_t frame, uint16_t row, uint16_t col);
