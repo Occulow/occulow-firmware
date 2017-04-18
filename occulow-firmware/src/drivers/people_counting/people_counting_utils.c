21178e16a95c0c4269ae4d2f07ae317df7212d09
Terence Sun <tsun1215@gmail.com>
2017-04-18 14:07:22 -0400

    Updated comments for people_counting

--- occulow-firmware/src/drivers/people_counting/people_counting_utils.c
+++ occulow-firmware/src/drivers/people_counting/people_counting_utils.c
@@ -8,6 +8,13 @@
 #include <drivers/people_counting/people_counting_utils.h>
 #include <drivers/grideye/grideye.h>
 
+/**
+ * @brief      Enqueues new_frame into frame_queue dropping the oldest frame
+ *
+ * @param      frame_queue  Queue to enqueue frame into
+ * @param[in]  new_frame    New frame
+ * @param[in]  queue_size   Length of queue
+ */
 void enqueue_frame(frame_t *frame_queue, frame_t new_frame, uint16_t queue_size) {
 	// Copy new frame data into frame_queue[0]
 	for (int i = 0; i < GE_FRAME_SIZE; i++) {
@@ -22,13 +29,28 @@ void enqueue_frame(frame_t *frame_queue, frame_t new_frame, uint16_t queue_size)
 	frame_queue[queue_size-1] = newest_frame;
 }
 
+/**
+ * @brief      Swaps two frame_elements
+ *
+ * @param      a     { parameter_description }
+ * @param      b     { parameter_description }
+ */
 void swap (frame_elem_t *a, frame_elem_t *b) {
 	frame_elem_t t = *a;
 	*a = *b;
 	*b = t;
 }
 
-int16_t partition (frame_t arr, int16_t l, int16_t h) {
+/**
+ * @brief      Partitions a frame for quicksort
+ *
+ * @param[in]  arr   List to partition
+ * @param[in]  l     Element to start partitioning from
+ * @param[in]  h     Element to stop partitioning at
+ *
+ * @return     Index of the partition
+ */
+int16_t partition(frame_t arr, int16_t l, int16_t h) {
 	frame_elem_t x = arr[h];
 	int16_t i = (l - 1);
 
@@ -42,7 +64,14 @@ int16_t partition (frame_t arr, int16_t l, int16_t h) {
 	return (i + 1);
 }
 
-void quick_sort (frame_t arr, int16_t l, int16_t h) {
+/**
+ * @brief      Performs quicksort on a slice of an array from l to h
+ *
+ * @param[in]  arr   Array to sort
+ * @param[in]  l     Index to start sorting from
+ * @param[in]  h     Index to stop sorting from
+ */
+void quick_sort(frame_t arr, int16_t l, int16_t h) {
     int16_t stack[h - l + 1];
     int16_t top = -1;
 
@@ -67,6 +96,15 @@ void quick_sort (frame_t arr, int16_t l, int16_t h) {
 }
 
 
+/**
+ * @brief      Gets the median value across all frames at the given index
+ *
+ * @param      frames      List of frames
+ * @param[in]  num_frames  Number of frames
+ * @param[in]  index       Index to find the median value for
+ *
+ * @return     The median value at the given index
+ */
 uint16_t median_at_index(frame_t *frames, uint16_t num_frames, uint16_t index) {
 	frame_elem_t temp_arr[num_frames];
 	// Copy elems into temp arr
@@ -85,6 +123,15 @@ uint16_t median_at_index(frame_t *frames, uint16_t num_frames, uint16_t index) {
 	}
 }
 
+/**
+ * @brief      Calculates the median frame
+ *
+ * @param[in]  frame_out   Output buffer to put median frame into
+ * @param      frames      List of frames
+ * @param[in]  num_frames  Number of frames
+ *
+ * @return     Median frame
+ */
 frame_t compute_median_frame(frame_t frame_out, frame_t *frames, uint16_t num_frames) {
 	// Find median for each element in the frame
 	for (int i = 0; i < GE_FRAME_SIZE; i++) {
@@ -94,6 +141,15 @@ frame_t compute_median_frame(frame_t frame_out, frame_t *frames, uint16_t num_fr
 }
 
 
+/**
+ * @brief      Returns if a given (row, col) in frame is a local maximum
+ *
+ * @param[in]  frame  Frame
+ * @param[in]  row    Row in frame
+ * @param[in]  col    Col in frame
+ *
+ * @return     True if local maximum, False otherwise.
+ */
 bool is_local_max(frame_t frame, uint16_t row, uint16_t col) {
 	frame_elem_t current_max = frame[GET_FRAME_INDEX(row, col)];
 
@@ -120,6 +176,14 @@ bool is_local_max(frame_t frame, uint16_t row, uint16_t col) {
 	return true;
 }
 
+/**
+ * @brief      Gets the index of the max in a column of frame
+ *
+ * @param[in]  frame  Frame to find a max in
+ * @param[in]  col    Column of frame to find max in
+ *
+ * @return     The index of the maximum value in the given column of frame
+ */
 uint16_t get_max_index_in_col(frame_t frame, uint16_t col) {
 	// Initialize max to the 0th element in the column
 	frame_elem_t max_elem = frame[GET_FRAME_INDEX(0, col)];
